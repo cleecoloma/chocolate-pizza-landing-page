0af86b1844efe8e1f45a9757d47a07398b3a289b
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-22 15:58:43 -0700

    completed lab

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const recipeElement = document.getElementById('input');
+// let liElement = document.getElementById('li');
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   liElement.setAttribute('li', '.clicked');
+//   console.log(liElement);
+// }
+
+// recipeElement.addEventListener('click', handleClick);
+
+function handleClick() {
+  // let liElement = document.getElementById('li');
+  // liElement.setAttribute('id', '.clicked');
+  document.getElementById('li').style.textDecoration = 'line-through';
+}
+
+const inputElement = document.getElementById('cb1');
+inputElement.onclick = function () {
+  handleClick();
+};
